c621bbe7730c2b9670a265ed03d174830b036bb5
beauregan745-cpu <beauregan745@gmail.com>
2026-03-14 20:05:34 +0000

    Create  script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNormal(){
+document.getElementById("normal").style.display="block";
+document.getElementById("everdark").style.display="none";
+}
+
+function showEverdark(){
+document.getElementById("normal").style.display="none";
+document.getElementById("everdark").style.display="block";
+}
